a89bd82426f4001e0b19e8253517c99e2d1a0b78
Jbrochan <hyeongchanj@gmail.com>
2022-10-31 16:14:49 +0900

    feat : Connect SignInActivity, SignUpActivity to MainActivity

--- inmysight-front/app/src/main/java/com/example/inmysight/MainActivity.kt
+++ inmysight-front/app/src/main/java/com/example/inmysight/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.inmysight
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val singInButton: Button = findViewById(R.id.signIn)
+
+        singInButton.setOnClickListener{
+            intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+        val signUpButton: Button = findViewById(R.id.signUp)
+        signUpButton.setOnClickListener {
+            intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- inmysight-front/app/src/main/java/com/example/inmysight/SignUpActivity.kt
+++ inmysight-front/app/src/main/java/com/example/inmysight/SignUpActivity.kt
@@ -3,9 +3,9 @@ package com.example.inmysight
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class LoginActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+        setContentView(R.layout.activity_signup)
     }
 }
