959d1840856856a99c690c941d2ef446bc67dc01
Jbrochan <hyeongchanj@gmail.com>
2022-11-18 14:19:43 +0900

    feat : Add cancel function to close button in activity_stock.xml

--- inmysight-front/app/src/main/java/com/example/inmysight/StockActivity.kt
+++ inmysight-front/app/src/main/java/com/example/inmysight/StockActivity.kt
@@ -2,10 +2,19 @@ package com.example.inmysight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StockActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stock)
+
+
+        // Move to lobby activity when press close button
+        val closeButton: Button = findViewById(R.id.stockCancelButton)
+        closeButton.setOnClickListener{
+            finish()
+        }
+
     }
 }
