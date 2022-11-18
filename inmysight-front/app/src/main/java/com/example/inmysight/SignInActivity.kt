312af2d9766616f24e725f32b967686b94050c1c
Jbrochan <hyeongchanj@gmail.com>
2022-11-18 19:18:40 +0900

    feat : Add sign-in function using firestore

--- inmysight-front/app/src/main/java/com/example/inmysight/SignInActivity.kt
+++ inmysight-front/app/src/main/java/com/example/inmysight/SignInActivity.kt
@@ -3,18 +3,40 @@ package com.example.inmysight
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
 
+        // Variables
+        val db = Firebase.firestore
+        var userId: String = ""    // User id
+        var userPassword: String = ""    // User password
+        var userCompany: String = ""    // User's company
+
         // Move to main activity when press sign-in button
         val signInButton: Button = findViewById(R.id.signInLogInButton)
         signInButton.setOnClickListener {
-            intent = Intent(this, LobbyActivity::class.java)
-            startActivity(intent)
+            // Store user id, password, company at variables from plain text
+            userId = findViewById<TextView>(R.id.signInIdInput).text.toString()
+            userPassword = findViewById<TextView>(R.id.signInPasswordInput).text.toString()
+            userCompany = findViewById<TextView>(R.id.signInCompanyInput).text.toString()
+
+            // Validate user's id, password, company from database
+            val userData = db.collection("root").document("company")
+                .collection("companies").document(userCompany)
+                .collection("users").document(userId).get()
+                .addOnSuccessListener {documentSnapshot ->
+                    if(userPassword == documentSnapshot["userPassword"]){
+                        intent = Intent(this, LobbyActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
         }
 
 
--- inmysight-front/app/src/main/java/com/example/inmysight/SignUpActivity.kt
+++ inmysight-front/app/src/main/java/com/example/inmysight/SignUpActivity.kt
@@ -6,11 +6,9 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlin.math.sign
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
@@ -18,24 +16,18 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signup)
 
         // Variables
-        // Firebase firestore database
-        val db = Firebase.firestore
-        // User id
-        var userId: String = ""
-        // User password
-        var userPassword: String = ""
-        // User's company
-        var userCompany: String = ""
-
+        val db = Firebase.firestore    // Firebase firestore database
+        var userId: String = ""    // User id
+        var userPassword: String = ""    // User password
+        var userCompany: String = ""    // User's company
 
         // Sign up function
         val signUpButton: Button = findViewById(R.id.signUpLogInButton)
         signUpButton.setOnClickListener {
-            // Store user id, password at variables from plain text
+            // Store user id, password, company at variables from plain text
             userId = findViewById<TextView>(R.id.signUpIdInput).text.toString()
             userPassword = findViewById<TextView>(R.id.signUpPasswordInput).text.toString()
             userCompany = findViewById<TextView>(R.id.signUpCompanyInput).text.toString()
-
             val userData = hashMapOf(
                 "userId" to userId,
                 "userPassword" to userPassword,
