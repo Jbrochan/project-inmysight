095144810645ae8c8708b4aa995c13dbd9038b71
Jbrochan <hyeongchanj@gmail.com>
2022-11-18 14:00:01 +0900

    feat : Change activity_lobby.xml to can see whole product

--- inmysight-front/app/src/main/java/com/example/inmysight/SignUpActivity.kt
+++ inmysight-front/app/src/main/java/com/example/inmysight/SignUpActivity.kt
@@ -2,14 +2,38 @@ package com.example.inmysight
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.sign
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
+        // Variables
+        // User id
+        var userId: String = ""
+        // User password
+        var userPassword: String = ""
+        // User's company
+        var userCompany: String = ""
+
+
         // Sign up function
+        val signUpButton: Button = findViewById(R.id.signUpLogInButton)
+        signUpButton.setOnClickListener {
+            // Store user id, password at variables from plain text
+            userId = findViewById<TextView>(R.id.signUpIdInput).text.toString()
+            userPassword = findViewById<TextView>(R.id.signUpPasswordInput).text.toString()
+            userCompany = findViewById<TextView>(R.id.signUpCompanyInput).text.toString()
+
+
+            // Send user's id, password, company to database
+            // If sign up success, send toast
+            Toast.makeText(applicationContext, "ID : $userId \nPassword : $userPassword \nCompany : $userCompany",Toast.LENGTH_SHORT).show()
+        }
 
         // Move to main activity when press close button
         val closeButton: Button = findViewById(R.id.signUpCancelButton)
