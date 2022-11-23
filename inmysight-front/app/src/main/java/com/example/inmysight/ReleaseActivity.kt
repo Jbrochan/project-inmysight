9978b2dd5c19cb8a1f6c9ea234434ee06fe5842d
Jbrochan <hyeongchanj@gmail.com>
2022-11-23 11:45:37 +0900

    feat : Add release activity, layout

---
+++ inmysight-front/app/src/main/java/com/example/inmysight/ReleaseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.inmysight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReleaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_release)
+    }
+}
