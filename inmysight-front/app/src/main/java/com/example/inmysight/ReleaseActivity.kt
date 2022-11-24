93e764229170cf7c838077c2354786b28c070f53
Jbrochan <hyeongchanj@gmail.com>
2022-11-24 15:17:47 +0900

    feat : Add release feature
    Add initial working release feature, finally fix bug when release.
    It was occurred because of typo in code, it fixed productAlarm to productAlert in dialogue part.
    But still remaining exception handling part like when user provide non-exist product name or null.

--- inmysight-front/app/src/main/java/com/example/inmysight/ReleaseActivity.kt
+++ inmysight-front/app/src/main/java/com/example/inmysight/ReleaseActivity.kt
@@ -1,7 +1,10 @@
 package com.example.inmysight
 
+import android.app.AlertDialog
 import android.content.ContentValues
 import android.content.ContentValues.TAG
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -31,6 +34,7 @@ class ReleaseActivity : AppCompatActivity() {
         var productStockCustomer: String    // Product's stock customer
         var productMemo: String    // Product's memo
         var productAlert: String    // Product's alert quantity
+        Log.d(ContentValues.TAG, "Present userCompany in ReleaseActivity is $userCompany")
 
         // Set releaseDateInput's text to today's date
         // now() method require min API level 26
@@ -39,7 +43,7 @@ class ReleaseActivity : AppCompatActivity() {
         // Release from fire-store database when click release button
         val releaseButton: Button = findViewById(R.id.releaseInputButton)
         releaseButton.setOnClickListener {
-            // Release from variables from plain text
+            // Store to variables from plain text
             productShelf = findViewById<TextView>(R.id.releaseShelfInput).text.toString()
             productName = findViewById<TextView>(R.id.releaseNameInput).text.toString()
             productQuantity = findViewById<TextView>(R.id.releaseQuantityInput).text.toString()
@@ -59,48 +63,59 @@ class ReleaseActivity : AppCompatActivity() {
                         val presentQuantity: Int = it.get("productQuantity").toString().toInt()
                         val finalQuantity: String = (presentQuantity - productQuantity.toInt()).toString()
                         Log.d(TAG, "Product quantity before release : $presentQuantity")
-                        Log.d(TAG, "Product quantity after release : $finalQuantity")
+                        Log.d(TAG, "Product quantity after release : ${finalQuantity.toInt()}")
+
+                        // Change to hash map for storing in database
+                        val productData = hashMapOf(
+                            "productShelf" to productShelf,
+                            "productName" to productName,
+                            "productQuantity" to finalQuantity,
+                            "productReleaseDate" to productStockDate,
+                            "productReleaseCustomer" to productStockCustomer,
+                            "productMemo" to productMemo,
+                            "productAlert" to productAlert
+                        )
+                        Log.d(TAG, "Hashmap 으로 변환 성공")
+                        Log.d(TAG, "변환 후 잔여량 : $finalQuantity")
 
                         // Release product if quantity of product is not zero
                         if(finalQuantity.toInt() > 0){
-                            // Change to hash map for storing in database
-                            val productData = hashMapOf(
-                                "productShelf" to productShelf,
-                                "productName" to productName,
-                                "productQuantity" to finalQuantity,
-                                "productReleaseDate" to productStockDate,
-                                "productReleaseCustomer" to productStockCustomer,
-                                "productMemo" to productMemo,
-                                "productAlert" to productAlert
-                            )
                             db.collection("root").document("company")
                                 .collection("companies").document(userCompany)
                                 .collection("product").document(productName).set(productData)
                                 .addOnSuccessListener {
-                                    Log.d(TAG, "${productName}, ${productQuantity}개 출고 성공")
-                                    Toast.makeText(this, "출고에 성공했습니다", Toast.LENGTH_LONG).show()
+                                    Toast.makeText(this, "출고에 성공했습니다.", Toast.LENGTH_LONG).show()
+                                }.addOnFailureListener {
+                                    Toast.makeText(this, "출고에 실패했습니다.", Toast.LENGTH_LONG).show()
                                 }
-                                .addOnFailureListener{
-                                    Log.d(TAG, "${productName}, ${productQuantity}개 출고 실패")
-                                    Toast.makeText(this, "출고에 실패하였습니다", Toast.LENGTH_LONG).show()
-                                }
-
-                        }
-                        // If quantity of product become 0, delete product in data base
-                        else if(finalQuantity.toInt() ==0){
+                        } else if(finalQuantity.toInt() == 0){
+                            // If quantity of product become 0, delete product in data base
                             db.collection("root").document("company")
                                 .collection("companies").document(userCompany)
                                 .collection("product").document(productName).delete()
-                        }
-                        // Require of releasing quantity is
-                        else{
-                            Toast.makeText(this, "잔여 수량을 초과하였습니다\n다시 작성 해주세요", Toast.LENGTH_LONG).show()
+                            Toast.makeText(this, "출고에 성공했으며\n잔여수량을 모두 출고하였습니다.", Toast.LENGTH_LONG).show()
+                        } else{
+                            // Require of releasing quantity is
+                            Toast.makeText(this, "잔여 수량을 초과하였습니다\n다시 작성 해주세요.", Toast.LENGTH_LONG).show()
                         }
 
+
                         // Send Alarm to user that product is re-ordered
-                        if(finalQuantity.toInt() <= it.get("productAlarm").toString().toInt()){
-                            Toast.makeText(this, "출고 후, 상품의 잔여량이 부족합니다\n재주문 해주세요", Toast.LENGTH_LONG).show()
+                        if(it.get("productQuantity").toString().toInt() <= it.get("productAlert").toString().toInt()){
+                            //Toast.makeText(this, "출고 후, 상품의 잔여량이 부족합니다\n재주문 해주세요", Toast.LENGTH_LONG).show()
+                            val builder = AlertDialog.Builder(this)
+                            builder
+                                .setTitle("재고 부족 알림")
+                                .setMessage("출고 후, 상품의 잔여량이 부족합니다\n" +
+                                        "재주문 해주세요")
+                                .setPositiveButton("확인",
+                                    DialogInterface.OnClickListener { dialog, id ->
+                                    })
+                            // Create the AlertDialog object and return it
+                            builder.create()
+                            builder.show()
                         }
+
 
                         // Set input space to initial value
                         findViewById<TextView>(R.id.releaseShelfInput).text = ""
@@ -110,6 +125,7 @@ class ReleaseActivity : AppCompatActivity() {
                         findViewById<TextView>(R.id.releaseCustomerInput).text = ""
                         findViewById<TextView>(R.id.releaseMemoInput).text = ""
                         findViewById<TextView>(R.id.releaseAlertInput).text = ""
+                        Log.d(TAG, "입력창 초기화 성공")
                     }
                     .addOnFailureListener{
                         Toast.makeText(this, "출고에 실패하였습니다", Toast.LENGTH_LONG).show()
