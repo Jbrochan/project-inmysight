9776a09d3763b79cf96b4493702fc1584e886581
Jbrochan <hyeongchanj@gmail.com>
2022-12-09 06:20:03 +0900

    feat : Prevent duplication when stocking product into database
    Prevent duplication when stocking product into database.
    It is not sure, when call firebase database the order of execution of code is reverse.

--- inmysight-front/app/src/main/java/com/example/inmysight/StockActivity.kt
+++ inmysight-front/app/src/main/java/com/example/inmysight/StockActivity.kt
@@ -31,8 +31,6 @@ class StockActivity : AppCompatActivity() {
         var productStockCustomer: String    // Product's stock customer
         var productMemo: String    // Product's memo
         var productAlert: String    // Product's alert quantity
-        lateinit var shelfList : ArrayList<String>    // Store list of shelf to check duplication of shelf
-        var shelfCheck = true    // To check duplication of shelf
         Log.d(ContentValues.TAG, "Present userCompany in StockActivity is $userCompany")
 
         // Set stockDateInput's text to today's date
@@ -52,7 +50,7 @@ class StockActivity : AppCompatActivity() {
             productAlert = findViewById<TextView>(R.id.stockAlertInput).text.toString()
 
             // Change to hash map for storing in database
-            val productData = hashMapOf(
+            var productData = hashMapOf(
                 "productShelf" to productShelf,
                 "productName" to productName,
                 "productQuantity" to productQuantity,
@@ -71,26 +69,78 @@ class StockActivity : AppCompatActivity() {
             val shelfData = hashMapOf(
                 "productShelf" to productShelf
             )
+            var productNameCheck = true
+            var presentQuantity: Int
+            lateinit var finalQuantity: String
 
             // Store in fire-store database from variables
             if (userCompany != null) {
-                // Store product
+                // Checking duplication
                 db.collection("root").document("company")
                     .collection("companies").document(userCompany)
-                    .collection("product").document(productName).set(productData)
+                    .collection("product").get()
                     .addOnSuccessListener {
-                        Toast.makeText(this, "입고에 성공했습니다.", Toast.LENGTH_LONG).show()
-                        findViewById<TextView>(R.id.stockShelfInput).text = ""
-                        findViewById<TextView>(R.id.stockNameInput).text = ""
-                        findViewById<TextView>(R.id.stockQuantityInput).text = ""
-                        findViewById<TextView>(R.id.stockDateInput).text = LocalDate.now().toString()
-                        findViewById<TextView>(R.id.stockCustomerInput).text = ""
-                        findViewById<TextView>(R.id.stockMemoInput).text = ""
-                        findViewById<TextView>(R.id.stockAlertInput).text = ""
+                        for(document in it){
+                            if(productName == document.get("productName")){
+                                presentQuantity = document.get("productQuantity").toString().toInt()
+                                finalQuantity = (presentQuantity + productQuantity.toInt()).toString()
+                                productNameCheck = false
+                                Log.d(TAG, "입고시 중복 제품 발견, 최종수량 : $presentQuantity + ${productQuantity.toInt()} = $finalQuantity")
+                                Log.d(TAG, "입고시 중복 제품 발견, 제품이름 ${document.get("productName")}")
+                            }
+                        }
+
+                        // Store product
+                        // If there are no duplication
+                        if(productNameCheck){
+                            db.collection("root").document("company")
+                                .collection("companies").document(userCompany)
+                                .collection("product").document(productName).set(productData)
+                                .addOnSuccessListener {
+                                    Toast.makeText(this, "입고에 성공했습니다.", Toast.LENGTH_LONG).show()
+                                    findViewById<TextView>(R.id.stockShelfInput).text = ""
+                                    findViewById<TextView>(R.id.stockNameInput).text = ""
+                                    findViewById<TextView>(R.id.stockQuantityInput).text = ""
+                                    findViewById<TextView>(R.id.stockDateInput).text = LocalDate.now().toString()
+                                    findViewById<TextView>(R.id.stockCustomerInput).text = ""
+                                    findViewById<TextView>(R.id.stockMemoInput).text = ""
+                                    findViewById<TextView>(R.id.stockAlertInput).text = ""
+                                }
+                                .addOnFailureListener{
+                                    Toast.makeText(this, "입고에 실패하였습니다.", Toast.LENGTH_LONG).show()
+                                }
+                        }
+                        // If there are duplication
+                        else{
+                            productData = hashMapOf(
+                                "productShelf" to productShelf,
+                                "productName" to productName,
+                                "productQuantity" to finalQuantity,
+                                "productStockDate" to productStockDate,
+                                "productStockCustomer" to productStockCustomer,
+                                "productMemo" to productMemo,
+                                "productAlert" to productAlert
+                            )
+                            db.collection("root").document("company")
+                                .collection("companies").document(userCompany)
+                                .collection("product").document(productName).set(productData)
+                                .addOnSuccessListener {
+                                    Toast.makeText(this, "입고에 성공했습니다.", Toast.LENGTH_LONG).show()
+                                    findViewById<TextView>(R.id.stockShelfInput).text = ""
+                                    findViewById<TextView>(R.id.stockNameInput).text = ""
+                                    findViewById<TextView>(R.id.stockQuantityInput).text = ""
+                                    findViewById<TextView>(R.id.stockDateInput).text = LocalDate.now().toString()
+                                    findViewById<TextView>(R.id.stockCustomerInput).text = ""
+                                    findViewById<TextView>(R.id.stockMemoInput).text = ""
+                                    findViewById<TextView>(R.id.stockAlertInput).text = ""
+                                }
+                                .addOnFailureListener{
+                                    Toast.makeText(this, "입고에 실패하였습니다.", Toast.LENGTH_LONG).show()
+                                }
+                        }
+
                     }
-                    .addOnFailureListener{
-                        Toast.makeText(this, "입고에 실패하였습니다.", Toast.LENGTH_LONG).show()
-                    }
+
 
                 // Store record
                 db.collection("root").document("company")
